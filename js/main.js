65b62c94c9ed411d0a4dd0f76fb0c9903645d963
Beavya <AnnBezymya@gmail.com>
2026-03-03 17:11:30 +0700

    Add component column

--- js/main.js
+++ js/main.js
@@ -1,10 +1,26 @@
+Vue.component('column', {
+    props: {
+        column: {
+            type: Object,
+            required: true
+        },
+    },
+    template: `
+        <div class="column">
+            <h2>{{ column.title }}</h2>
+            <div class="cards-container">
+            </div>
+        </div>
+    `
+})
+
 let app = new Vue({
     el: '#app',
     data: {
         columns: [
-            { id: 1, title: "To Do (max 3)", limit: 3 },
-            { id: 2, title: "In Progress (max 5)", limit: 5 },
-            { id: 3, title: "Done (unlimited)", limit: null }
-        ]
+            { id: 1, title: 'To Do (max 3)', limit: 3 },
+            { id: 2, title: 'In Progress (max 5)', limit: 5 },
+            { id: 3, title: 'Done (unlimited)', limit: null }
+        ],
     }
 })
